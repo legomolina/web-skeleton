11357ffa4d989efb1a9c8d67e89c9f15bfde5ec6
legomolina <legomolina@gmail.com>
2017-01-19 13:11:00 +0100

    Updated gulpfile.js.

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,22 +8,22 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('sass', function() {
-    return gulp.src('public_html/style/scss/style.scss')
+    return gulp.src('public_html/styles/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public_html/style/css'))
+        .pipe(gulp.dest('public_html/styles/css'))
 });
 
 gulp.task('styles', ['sass'], function() {
-    var stream = gulp.src('public_html/style/css/style.css')
+    var stream = gulp.src('public_html/styles/css/style.css')
         .pipe(concat('style.css'))
         .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('public_html/style/css'))
+        .pipe(gulp.dest('public_html/styles/css'))
         .pipe(livereload());
 });
 
 gulp.task('watch', ['styles'], function () {
     livereload.listen();
-    gulp.watch(['public_html/style/scss/**/*.scss', 'public_html/style/css/**/*.css'], ['styles']);
+    gulp.watch(['public_html/styles/scss/**/*.scss', 'public_html/styles/css/**/*.css'], ['styles']);
     gulp.watch(['app/**/*.*', 'public_html/**/*.*', 'uploads/**/*.*']).on('change', function(file) {livereload.changed(file.path);});
 });
 
